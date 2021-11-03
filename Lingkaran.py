e53c9af056e95ac92d86b9d2f8c5e2ec0e8919aa
Indira Rully <navrida.indira@gmail.com>
2021-11-03 10:54:53 +0700

    menambahkan file

---
+++ Lingkaran.py
@@ -0,0 +1,9 @@
+
+r = int(input("Masukkan Jari-Jari (r) : "))
+
+L = float(22) / 7 * r * r
+K = 2 * (float(22) / 7) *  r
+
+print ("Luas (L) lingkaran : " + str(L))
+print ("Keliling (K) lingkaran : " + str(K))
+
